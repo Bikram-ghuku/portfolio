0313ff11037d7949fc1829033fd9ffe674e30bf3
Bikram Ghuku <bikramghuku05@gmail.com>
2023-09-26 23:38:24 +0530

    creation of intro

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './intro.css'
+function Intro() {
+  return (
+    <div className='i'>
+        <div className="i-left">
+            <div className="i-left-wwrapper">
+                <h2 className="i-intro">Hello, My name is</h2>
+                <h1 className="i-name">Bikram Ghuku</h1>
+            </div>
+        </div>
+        <div className="i-right">Right</div>
+    </div>
+  )
+}
+
+export default Intro
