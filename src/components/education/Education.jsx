79a0f0d4a778216adf87dee9a6cdfa1d2d626623
Bikram Ghuku <bikramghuku05@gmail.com>
2023-09-29 18:35:52 +0530

    updated education page to read data from json

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './education.css'
+import { education_data } from './education_data.js'
+
 
 function Education2() {
   return (
@@ -8,28 +10,13 @@ function Education2() {
             Education
         </h1>
         <div className="e-wrapper">
-            <div className="e-card">
-                <h2 className="e-card-title">
-                    DAV Model School, IIT Kharagpur
-                </h2>
-                <p className="e-card-desc">
-                    Physics + Chemistry + Mathematics + Computer Science + Phy Education, High School Diploma
-                </p>
-                <h3 className="e-card-subtitle">
-                    2016 - 2023
-                </h3>
-            </div>
-            <div className="e-card">
-                <h2 className="e-card-title">
-                    Indian Institue of Technology, Kharagpur
-                </h2>
-                <p className="e-card-desc">
-                    Computer Science and Engineering, IIT Kharagpur Dual Degeree (Bachelors Of Technology B.Tech + Masters Of Technology M.Tech)
-                </p>
-                <h3 className="e-card-subtitle">
-                    2023 - 2028
-                </h3>
-            </div>
+            {education_data.map((data, key) => (
+                <div className="e-card">
+                    <h2 className='e-card-titel'>{data.Name}</h2>
+                    <p className='e-card-desc'>{data.Desc}</p>
+                    <h3 className='e-card-subtitle'>{data.Years}</h3>
+                </div>
+            ))}
         </div>
     </div>
   )
---
+++ src/components/education/education_data.js
@@ -0,0 +1,19 @@
+export const education_data=[
+    {
+        id:1,
+        Name: "DAV Model School, IIT KGP",
+        Desc: "Middle School Diploma, AISSE",
+        Years: "2016-2021"
+    },
+    {
+        id:2,
+        Name: "DAV Model School, IIT KGP",
+        Desc: "Physics + Chemistry + Mathematics + Computer Science + Phy Education, High School Diploma",
+        Years: "2021-2023"
+    },{
+        id:3,
+        Name: "Indian Institute of Technology Kharagpur",
+        Desc: "Computer Science and Engineering, IIT Kharagpur Dual Degeree (Bachelors Of Technology B.Tech + Masters Of Technology M.Tech)",
+        Years: "2023-2028"
+    }
+]
